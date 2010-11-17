2f2b1365fa4a6724efd69319d3368d86f083bba6
Pekka T <pekka.t.kanerva@gmail.com>
2010-11-17 13:01:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkingLot::Application.config.secret_token = 'ca3b5d6d247e92b32dc56efaaeab216773e529b366e62bb9b1fff81d8a82957e3f2db2b212ead6c8df0a1f467920363f90614edba4f6cfad9cb43a5795a6589d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ParkingLot::Application.config.session_store :cookie_store, :key => '_parking_lot_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ParkingLot::Application.config.session_store :active_record_store
